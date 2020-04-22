f227844e468775fe2cc9763967cf269b9cd2fc74
zanebliss <zanebliss@icloud.com>
2020-04-22 08:22:50 -0500

    updated to part 2

---
+++ scripts/journal.js
@@ -0,0 +1,32 @@
+const journal = []
+const journalEntry_1 = {
+    date: "4/14/20",
+    concepts: "Figma and wireframing, navigation, communication",
+    entry: "",
+    mood: "Happy"
+}
+journal.push(journalEntry_1)
+
+const journalEntry_2 = {
+    date: "4/15/20",
+    concepts: "Figma and wireframing, navigation, communication",
+    entry: "",
+    mood: "Happy"
+}
+journal.push(journalEntry_2)
+
+const journalEntry_3 = {
+    date: "4/16/20",
+    concepts: "Presenting, introduction to JS",
+    entry: "",
+    mood: "Happy"
+}
+journal.push(journalEntry_3)
+
+const journalEntry_4 = {
+    date: "4/17/20",
+    concepts: "Datatypes, objects, DOM",
+    entry: "",
+    mood: "Happy"
+}
+journal.push(journalEntry_4)
